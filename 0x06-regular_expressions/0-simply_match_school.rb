bad656373ae99b4b866470b8a8d6c6e283165aee
Abenezer Tekalign <abenezertekalign12@gmail.com>
2023-05-03 13:18:42 -0700

    TASKS

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#This script shows argument and pass and also a regular expression that is to matching School
+
+#puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# Token #1
+
+# puts ARGV[0].scan(/hb?tn/).join
+# OR
+
+puts ARGV[0].scan(/^hb?tn/).joini
